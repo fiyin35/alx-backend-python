e299f3b28fcbf446a59fcdcfe47df5fe01a6b634
fiyin <lolafiyin@gmail.com>
2025-07-27 21:45:03 +0100

    modify the chats/views.py, add 403 forbidden to perform_create function

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,34 +1,12 @@
 from rest_framework import viewsets, status, filters
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.exceptions import PermissionDenied, ValidationError
+from rest_framework.exceptions import ValidationError
 from django_filters.rest_framework import DjangoFilterBackend
-from .permissions import IsOwnerOrReadOnly
-
+from rest_framework.status import HTTP_403_FORBIDDEN
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
-
-
-class ConversationViewSet(viewsets.ModelViewSet):
-    serializer_class = ConversationSerializer
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
-    filter_backends = [filters.OrderingFilter]
-    ordering_fields = ['created_at']
-
-    def get_queryset(self):
-        return Conversation.objects.filter(participants=self.request.user).prefetch_related('participants', 'messages')
-
-    def create(self, request, *args, **kwargs):
-        participant_ids = request.data.get('participants')
-        if not participant_ids:
-            raise ValidationError({'participants': 'This field is required.'})
-
-        conversation = Conversation.objects.create()
-        conversation.participants.set(participant_ids + [request.user.id])
-        conversation.save()
-
-        serializer = self.get_serializer(conversation)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+from .permissions import IsOwnerOrReadOnly
 
 
 class MessageViewSet(viewsets.ModelViewSet):
@@ -39,16 +17,24 @@ class MessageViewSet(viewsets.ModelViewSet):
     ordering_fields = ['timestamp']
 
     def get_queryset(self):
-        queryset = Message.objects.select_related('sender', 'conversation')
-        return queryset.filter(conversation__participants=self.request.user)
+        return Message.objects.filter(
+            conversation__participants=self.request.user
+        ).select_related('sender', 'conversation')
 
     def perform_create(self, serializer):
         conversation_id = self.request.data.get('conversation')
         if not conversation_id:
             raise ValidationError({'conversation': 'This field is required.'})
 
-        conversation = Conversation.objects.filter(id=conversation_id, participants=self.request.user).first()
+        conversation = Conversation.objects.filter(
+            id=conversation_id,
+            participants=self.request.user
+        ).first()
+
         if not conversation:
-            raise PermissionDenied('You are not a participant in this conversation.')
+            return Response(
+                {"detail": "You are not a participant in this conversation."},
+                status=HTTP_403_FORBIDDEN
+            )
 
         serializer.save(sender=self.request.user, conversation=conversation)
